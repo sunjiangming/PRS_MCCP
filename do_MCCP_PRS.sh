d44da27e9514fbef5cbc24bc9bb1eedd4957bdf0
Jiangming Sun <sunjiangming@gmail.com>
2020-04-20 14:09:49 +0200

    Update do_MCCP_PRS.sh

--- do_MCCP_PRS.sh
+++ do_MCCP_PRS.sh
@@ -1,18 +1,20 @@
 module load R/3.4.2
 #set working directory, dir contains PRS profile
 wdir=$1
-#input file name
-ifile=$2
+#input file for training and clibration -label-known set.
+tr_file=$2
+#input file for predicting -label-unknown set.
+te_file=$3
 # column number for phenotype
-y_col=$3
+y_col=$4
 # column number for PRS
-prs_col=$4
+prs_col=$5
 # column number for covariates
-covar_col=$5
+covar_col=$6
 # output file name
-ofile=$6
+ofile=$7
 
 #without any covariates
-Rscript MCCP_PRS.R $wdir $ifile  $y_col $prs_col 'c()' $ofile
+Rscript MCCP_PRS.R $wdir $tr_file $te_file $y_col $prs_col 'c()' $ofile
 #single with covariates
-Rscript MCCP_PRS.R $wdir $ifile  $y_col $prs_col 'c("$covar_col")' $ofile
+Rscript MCCP_PRS.R $wdir $tr_file $te_file $y_col $prs_col 'c("$covar_col")' $ofile
